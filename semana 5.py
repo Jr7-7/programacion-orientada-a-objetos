ff9e2708354843990a274858c71722c651f65d28
johana grefa <johanagrefa3@gmail.com>
2024-06-30 22:47:44 -0500

    Se utiliza una secuencia de instrucciones para recibir los valores de entrada, realizar las operaciones matemáticas y mostrar el resultado.
    El código podría verse así:

---
+++ semana 5.py
@@ -0,0 +1,13 @@
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print("La suma es:", suma)
+print("La resta es:", resta)
+print("La multiplicación es:", multiplicacion)
+print("La división es:", division)
+print('esta es una mini calculadora, muy simple')
