3266734f3cd1657a468bd50c3d61940d5073bc8b
semana 2 <johanagrefa3@gmail.com>
2024-08-30 20:44:14 -0500

    MEJORANDO ESTE INVENTARIO SEM 11 GESTION DE INVENTARIO

---
+++ sem11. gestion de inventario.py
@@ -0,0 +1,30 @@
+class Producto:
+    def __init__(self, id_producto, nombre, cantidad, precio):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+    def obtener_id(self):
+        return self.id_producto
+
+    def establecer_id(self, id_producto):
+        self.id_producto = id_producto
+
+    def obtener_nombre(self):
+        return self.nombre
+
+    def establecer_nombre(self, nombre):
+        self.nombre = nombre
+
+    def obtener_cantidad(self):
+        return self.cantidad
+
+    def establecer_cantidad(self, cantidad):
+        self.cantidad = cantidad
+
+    def obtener_precio(self):
+        return self.precio
+
+    def establecer_precio(self, precio):
+        self.precio = precio
